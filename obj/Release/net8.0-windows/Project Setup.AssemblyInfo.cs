e46cec793a11b9ac28163c2b812fcb3720e4ab1d
MagicalMongoose <drew1681@gmail.com>
2025-03-28 22:37:33 -0500

    Built an EXE!

--- obj/Release/net8.0-windows/Project Setup.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/Project Setup.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project Setup")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14dce73c55209ffe27500b3987111538c9e67508")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project Setup")]
